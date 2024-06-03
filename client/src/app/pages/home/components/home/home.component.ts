de0bf9f8a82c7cb38afd62236e43b8675780efab
yancostrishevsky <bblink999@gmail.com>
2024-06-03 21:33:32 +0200

    minor changes

--- client/src/app/pages/home/components/home/home.component.ts
+++ client/src/app/pages/home/components/home/home.component.ts
@@ -9,12 +9,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 })
 export class HomeComponent {
   featuredDestinations = [
-    { name: 'Destination 1', image: 'assets/stock-photo-beautiful-beach-on-hong-island-krabi-thailand-landmark-destination-southeast-asia-travel-2190416887.jpg' },
-    { name: 'Destination 2', image: 'assets/stock-photo-summer-travel-vacation-concept-happy-traveler-asian-woman-relax-and-sightseeing-on-beach-in-2147728737.jpg' },
-    { name: 'Destination 3', image: 'assets/stock-photo-summer-travel-vacation-concept-happy-traveler-asian-woman-with-dress-relax-and-sightseeing-on-1951840408.jpg'},
-    { name: 'Destination 4', image: 'assets/stock-photo-beautiful-beach-on-hong-island-krabi-thailand-landmark-destination-southeast-asia-travel-2190416887.jpg' },
-    { name: 'Destination 5', image: 'assets/stock-photo-summer-travel-vacation-concept-happy-traveler-asian-woman-with-hat-relax-in-hammock-on-beach-in-2306404091.jpg' },
-    { name: 'Destination 6', image: 'assets/stock-photo-summer-travel-vacation-concept-happy-traveler-asian-woman-with-hat-relax-in-hammock-on-beach-in-1906694002.jpg'},
+    { name: 'Destination 1', image: 'assets/image1.png' },
+    { name: 'Destination 2', image: 'assets/image2.jpeg' },
+    { name: 'Destination 3', image: 'assets/image3.jpeg'},
+    { name: 'Destination 4', image: 'assets/image4.jpeg' },
+    { name: 'Destination 5', image: 'assets/image5.jpeg' },
+
 
   ];
 }
